fd84218fa3cd65e38bbd33d60ea35c2ef1b53843
suhail <suhail.r@hubspire.com>
2024-01-16 21:14:10 +0530

    Initialized typescript and material ui configuration, eslint config, theme files

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          hii
+        </ThemeProvider>
+      </div>
+    </>
+  );
+}
+
+export default App;
